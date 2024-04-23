0b16ac2390f4ba473c2274582c104e2c2e766cef
havanchau <chauhvc2.dy@gmail.com>
2024-04-24 06:12:04 +0700

    add: config devide

--- src/screens/infoQuat.js
+++ src/screens/infoQuat.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from "react";
 import { View, Text, TextInput, Image, TouchableOpacity } from "react-native";
-import axios from 'axios';
-import LottieView from 'lottie-react-native';
-import { TailwindProvider } from 'tailwindcss-react-native';
-import { useNavigation } from '@react-navigation/native';
+import axios from "axios";
+import LottieView from "lottie-react-native";
+import { TailwindProvider } from "tailwindcss-react-native";
+import { useNavigation } from "@react-navigation/native";
 
 import logo from "../assets/logo.png";
 import homeJson from "../assets/home_1.json";
@@ -16,8 +16,6 @@ export default infoQuat = () => {
   const [sensorValue, setSensorValue] = useState(null); // Thêm state để lưu giá trị từ cambien1
   const [lightValue, setLightValue] = useState(null); // Thêm state để lưu giá trị từ cambien2
   const [humiValue, setHumiValue] = useState(null); // Thêm state để lưu giá trị từ cambien
-  const username = process.env.USERNAME;
-  const key = process.env.PASSWORD;
 
   useEffect(() => {
     let interval = null;
@@ -52,20 +50,22 @@ export default infoQuat = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
+        const username = process.env.USERNAME;
+        const key = process.env.PASSWORD;
         const response = await axios.get(
-          'https://io.adafruit.com/api/v2/thanhliemtala/feeds/button2',
+          "https://io.adafruit.com/api/v2/thanhliemtala/feeds/button2",
           {
             headers: {
-              'X-AIO-Key': key,
-              'Content-Type': 'application/json',
+              "X-AIO-Key": key,
+              "Content-Type": "application/json",
             },
           }
         );
 
         const value = response.data.last_value; // Lấy giá trị mới nhất từ Adafruit
 
-
-        if (value !== prevMode) { // So sánh giá trị mới với giá trị trước đó
+        if (value !== prevMode) {
+          // So sánh giá trị mới với giá trị trước đó
           // Cập nhật giá trị vào trạng thái của ứng dụng
           if (value === "0") {
             // mode = "TẮT";
@@ -77,27 +77,27 @@ export default infoQuat = () => {
             // mode = "MỨC 2";
             setMode("MỨC 2");
           } else if (value === "3") {
-          //  mode = "MỨC 3";
+            //  mode = "MỨC 3";
             setMode("MỨC 3");
           }
         }
 
         setPrevMode(value); // Cập nhật giá trị mới vào biến trạng thái lưu giữ giá trị trước đó
-
-
       } catch (error) {
-        console.error('Lỗi khi lấy dữ liệu từ Adafruit:', error);
+        console.error("Lỗi khi lấy dữ liệu từ Adafruit:", error);
       }
     };
 
     const fetchSensorData = async () => {
       try {
+        const username = process.env.USERNAME;
+        const key = process.env.PASSWORD;
         const response = await axios.get(
-          'https://io.adafruit.com/api/v2/thanhliemtala/feeds/cambien1',
+          "https://io.adafruit.com/api/v2/thanhliemtala/feeds/cambien1",
           {
             headers: {
-              'X-AIO-Key': key,
-              'Content-Type': 'application/json',
+              "X-AIO-Key": key,
+              "Content-Type": "application/json",
             },
           }
         );
@@ -106,19 +106,20 @@ export default infoQuat = () => {
 
         setSensorValue(value);
       } catch (error) {
-        console.error('Lỗi khi lấy dữ liệu từ Adafruit:', error);
+        console.error("Lỗi khi lấy dữ liệu từ Adafruit:", error);
       }
     };
 
-
     const fetchLightData = async () => {
       try {
+        const username = process.env.USERNAME;
+        const key = process.env.PASSWORD;
         const response = await axios.get(
-          'https://io.adafruit.com/api/v2/thanhliemtala/feeds/cambien2',
+          "https://io.adafruit.com/api/v2/thanhliemtala/feeds/cambien2",
           {
             headers: {
-              'X-AIO-Key': key,
-              'Content-Type': 'application/json',
+              "X-AIO-Key": key,
+              "Content-Type": "application/json",
             },
           }
         );
@@ -127,18 +128,20 @@ export default infoQuat = () => {
 
         setLightValue(value);
       } catch (error) {
-        console.error('Lỗi khi lấy dữ liệu từ Adafruit:', error);
+        console.error("Lỗi khi lấy dữ liệu từ Adafruit:", error);
       }
     };
 
     const fetchHumiData = async () => {
       try {
+        const username = process.env.USERNAME;
+        const key = process.env.PASSWORD;
         const response = await axios.get(
-          'https://io.adafruit.com/api/v2/thanhliemtala/feeds/cambien3',
+          "https://io.adafruit.com/api/v2/thanhliemtala/feeds/cambien3",
           {
             headers: {
-              'X-AIO-Key': key,
-              'Content-Type': 'application/json',
+              "X-AIO-Key": key,
+              "Content-Type": "application/json",
             },
           }
         );
@@ -147,11 +150,9 @@ export default infoQuat = () => {
 
         setHumiValue(value);
       } catch (error) {
-        console.error('Lỗi khi lấy dữ liệu từ Adafruit:', error);
+        console.error("Lỗi khi lấy dữ liệu từ Adafruit:", error);
       }
     };
-
-    
 
     fetchData(); // Gọi hàm fetchData lần đầu khi component được render
     fetchSensorData(); // Gọi hàm fetchSensorData lần đầu khi component được render
@@ -206,100 +207,103 @@ export default infoQuat = () => {
   const sendDataToAdafruit = async (value) => {
     try {
       const response = await axios.post(
-        'https://io.adafruit.com/api/v2/thanhliemtala/feeds/button2/data',
+        "https://io.adafruit.com/api/v2/thanhliemtala/feeds/button2/data",
         {
           value: value,
         },
         {
           headers: {
-            'X-AIO-Key': key,
-            'Content-Type': 'application/json',
+            "X-AIO-Key": key,
+            "Content-Type": "application/json",
           },
         }
       );
 
       // Xử lý dữ liệu nhận được từ Adafruit
-      console.log('Dữ liệu từ Adafruit:', response.data);
+      console.log("Dữ liệu từ Adafruit:", response.data);
     } catch (error) {
-      console.error('Lỗi khi gửi dữ liệu tới Adafruit:', error);
+      console.error("Lỗi khi gửi dữ liệu tới Adafruit:", error);
     }
   };
 
   return (
     <TailwindProvider>
-      <View className='flex-1 items-center pb-10 pt-10'>
-        <Text className='font-bold text-2xl'>Thông tin chi tiết</Text>
-        <Image
-          source={logo}
-          className="w-[140] h-[70]"
-        />
-        <LottieView
-          source={homeJson}
-          autoPlay
-          loop={true}
-        />
-        <Text className='font-bold text-xl mt-4'>Chế độ: {mode}</Text>
-        <Text className='font-bold text-xl'>Thời gian: {formatTime(timer)}</Text>
+      <View className="flex-1 items-center pb-10 pt-10">
+        <Text className="font-bold text-2xl">Thông tin chi tiết</Text>
+        <Image source={logo} className="w-[140] h-[70]" />
+        <LottieView source={homeJson} autoPlay loop={true} />
+        <Text className="font-bold text-xl mt-4">Chế độ: {mode}</Text>
+        <Text className="font-bold text-xl">
+          Thời gian: {formatTime(timer)}
+        </Text>
 
         <TouchableOpacity
           onPress={toggleMode}
           style={{
-            alignItems: 'center',
+            alignItems: "center",
             paddingVertical: 30,
             marginTop: 30,
             paddingHorizontal: 60,
             marginBottom: 30,
-            backgroundColor: 'blue',
+            backgroundColor: "blue",
             borderRadius: 30,
           }}
         >
-        <Text style={{ fontSize: 18, color: 'white', fontWeight: 'bold' }}>Chuyển đổi chế độ</Text>
+          <Text style={{ fontSize: 18, color: "white", fontWeight: "bold" }}>
+            Chuyển đổi chế độ
+          </Text>
         </TouchableOpacity>
 
         <TouchableOpacity
           style={{
-            alignItems: 'center',
+            alignItems: "center",
             paddingVertical: 30,
             paddingHorizontal: 60,
             marginTop: 8,
             marginBottom: 8,
-            backgroundColor: 'blue',
+            backgroundColor: "blue",
             borderRadius: 30,
-            width: '70%',
+            width: "70%",
           }}
         >
-          <Text style={{ fontSize: 18, color: 'white', fontWeight: 'bold' }}>Nhiệt độ: {sensorValue} °C</Text>
+          <Text style={{ fontSize: 18, color: "white", fontWeight: "bold" }}>
+            Nhiệt độ: {sensorValue} °C
+          </Text>
         </TouchableOpacity>
 
         <TouchableOpacity
           style={{
-            alignItems: 'center',
+            alignItems: "center",
             paddingVertical: 30,
             paddingHorizontal: 60,
             marginTop: 8,
             marginBottom: 8,
-            backgroundColor: 'blue',
+            backgroundColor: "blue",
             borderRadius: 30,
-            width: '70%',
+            width: "70%",
           }}
         >
-          <Text style={{ fontSize: 18, color: 'white', fontWeight: 'bold' }}>Ánh sáng: {lightValue} lux</Text>
+          <Text style={{ fontSize: 18, color: "white", fontWeight: "bold" }}>
+            Ánh sáng: {lightValue} lux
+          </Text>
         </TouchableOpacity>
 
         <TouchableOpacity
           style={{
-            alignItems: 'center',
+            alignItems: "center",
             paddingVertical: 30,
             paddingHorizontal: 60,
             marginTop: 8,
-            backgroundColor: 'blue',
+            backgroundColor: "blue",
             borderRadius: 30,
-            width: '70%',
+            width: "70%",
           }}
         >
-          <Text style={{ fontSize: 18, color: 'white', fontWeight: 'bold' }}>Độ ẩm: {humiValue} %</Text>
+          <Text style={{ fontSize: 18, color: "white", fontWeight: "bold" }}>
+            Độ ẩm: {humiValue} %
+          </Text>
         </TouchableOpacity>
-              </View>
-            </TailwindProvider>
+      </View>
+    </TailwindProvider>
   );
 };
